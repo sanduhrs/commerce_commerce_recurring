c382187b2869d5ddeae8e4a84b5f2d0d11dc4fbc
bojanz <bojanz@86106.no-reply.drupal.org>
2017-12-20 14:17:16 +0100

    Issue #2924297 by bojanz, joachim, rszrama: Allow fixed billing periods to start on a specific day/month

--- tests/src/Kernel/Plugin/Commerce/BillingSchedule/FixedTest.php
+++ tests/src/Kernel/Plugin/Commerce/BillingSchedule/FixedTest.php
@@ -71,4 +71,78 @@ class FixedTest extends KernelTestBase {
     $this->assertEquals(new DrupalDateTime('2019-01-01 00:00:00'), $next_billing_period->getEndDate());
   }
 
+  /**
+   * @covers ::generateFirstBillingPeriod
+   */
+  public function testCustomStart() {
+    // Hourly intervals should be unaffected.
+    $plugin = new Fixed([
+      'interval' => [
+        'number' => '2',
+        'unit' => 'hour',
+      ],
+      'start_month' => '2',
+      'start_day' => '3',
+    ], '', []);
+    $start_date = new DrupalDateTime('2017-03-16 10:22:30');
+    $billing_period = $plugin->generateFirstBillingPeriod($start_date);
+    $this->assertEquals(new DrupalDateTime('2017-03-16 10:00:00'), $billing_period->getStartDate());
+    $this->assertEquals(new DrupalDateTime('2017-03-16 12:00:00'), $billing_period->getEndDate());
+
+    // Monthly intervals should ignore the start_month.
+    $plugin = new Fixed([
+      'interval' => [
+        'number' => '1',
+        'unit' => 'month',
+      ],
+      'start_month' => '2',
+      'start_day' => '3',
+    ], '', []);
+    $start_date = new DrupalDateTime('2017-03-16 10:22:30');
+    $billing_period = $plugin->generateFirstBillingPeriod($start_date);
+    $this->assertEquals(new DrupalDateTime('2017-03-03 00:00:00'), $billing_period->getStartDate());
+    $this->assertEquals(new DrupalDateTime('2017-04-03 00:00:00'), $billing_period->getEndDate());
+
+    // Subscription started before start_day.
+    $plugin = new Fixed([
+      'interval' => [
+        'number' => '1',
+        'unit' => 'month',
+      ],
+      'start_month' => '2',
+      'start_day' => '3',
+    ], '', []);
+    $start_date = new DrupalDateTime('2017-03-02 10:22:30');
+    $billing_period = $plugin->generateFirstBillingPeriod($start_date);
+    $this->assertEquals(new DrupalDateTime('2017-02-03 00:00:00'), $billing_period->getStartDate());
+    $this->assertEquals(new DrupalDateTime('2017-03-03 00:00:00'), $billing_period->getEndDate());
+
+    $plugin = new Fixed([
+      'interval' => [
+        'number' => '2',
+        'unit' => 'year',
+      ],
+      'start_month' => '2',
+      'start_day' => '3',
+    ], '', []);
+    $start_date = new DrupalDateTime('2017-03-16 10:22:30');
+    $billing_period = $plugin->generateFirstBillingPeriod($start_date);
+    $this->assertEquals(new DrupalDateTime('2017-02-03 00:00:00'), $billing_period->getStartDate());
+    $this->assertEquals(new DrupalDateTime('2019-02-03 00:00:00'), $billing_period->getEndDate());
+
+    // Subscription started before start_day/start_month.
+    $plugin = new Fixed([
+      'interval' => [
+        'number' => '2',
+        'unit' => 'year',
+      ],
+      'start_month' => '2',
+      'start_day' => '3',
+    ], '', []);
+    $start_date = new DrupalDateTime('2017-01-16 10:22:30');
+    $billing_period = $plugin->generateFirstBillingPeriod($start_date);
+    $this->assertEquals(new DrupalDateTime('2015-02-03 00:00:00'), $billing_period->getStartDate());
+    $this->assertEquals(new DrupalDateTime('2017-02-03 00:00:00'), $billing_period->getEndDate());
+  }
+
 }
